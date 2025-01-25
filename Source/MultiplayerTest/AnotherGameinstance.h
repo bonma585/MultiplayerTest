cf73adc2a3dd065bc3b2a3347cc3181bf03c68d2
YE Chenyi <ye-ch564@school.lu>
2025-01-25 14:12:22 +0100

    Test

--- Source/MultiplayerTest/AnotherGameinstance.h
+++ Source/MultiplayerTest/AnotherGameinstance.h
@@ -1,10 +1,67 @@
+/*
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+#include "OnlineSubsystem.h"
+#include "OnlineSessionSettings.h"
+#include "Interfaces/OnlineSessionInterface.h"
+
+#include "Engine/GameInstance.h"
+#include "AnotherGameinstance.generated.h"
+
+/**
+ * 
+ 
+UCLASS()
+class MULTIPLAYERTEST_API UAnotherGameinstance : public UGameInstance
+{
+    GENERATED_BODY()
+
+public:
+    UAnotherGameinstance();
+
+protected:
+
+    TSharedPtr<FOnlineSessionSearch> SessionSearch;
+
+    IOnlineSessionPtr SessionInterface;
+
+    FName SESSION_NAME = FName("My Game");
+
+    FName testKey = FName("ServerKey");
+    FName testValue = FName("ServerName");
+
+
+
+    void Init() override;
+
+    UFUNCTION(BlueprintCallable)
+    void CreateServer();
+    UFUNCTION(BlueprintCallable)
+    void JoinServer();
+
+    //Delegates
+    void OnCreateSessionComplete(FName SessionName, bool wasSuccessful);
+    void OnFindSessionComplete(bool Succeeded);
+    void OnJoinSessionComplete(FName SessionName, EOnJoinSessionCompleteResult::Type Result);
+    void OnDestroySessionComplete(FName SessionName, bool bWasSuccessful);
+
+
+
+
+};
+*/
+
 #pragma once
 
 #include "CoreMinimal.h"
 #include "Engine/GameInstance.h"
+#include "OnlineSubsystem.h"
+#include "OnlineSessionSettings.h"
 #include "Interfaces/OnlineSessionInterface.h"
-
-#include "OnlineSubsystem.h" 
 #include "AnotherGameInstance.generated.h"
 
 USTRUCT(BlueprintType)
@@ -25,13 +82,10 @@ struct FServerData
     int32 MaxPlayers;
 };
 
-
-
 UCLASS()
 class MULTIPLAYERTEST_API UAnotherGameInstance : public UGameInstance
 {
     GENERATED_BODY()
-
 
 public:
     UAnotherGameInstance();
@@ -42,11 +96,12 @@ public:
     void Host(const FString& ServerName);
 
     UFUNCTION(BlueprintCallable)
-    void Join(int32 ServerIndex);
-
-    UFUNCTION(BlueprintCallable)
     void FindSessions();
 
+    UFUNCTION(BlueprintCallable)
+    void Join(int32 ServerIndex);
+
+    // Callbacks
     UFUNCTION()
     void OnCreateSessionComplete(FName SessionName, bool bWasSuccessful);
 
@@ -56,17 +111,16 @@ public:
     UFUNCTION()
     void OnFindSessionsComplete(bool bWasSuccessful);
 
+    UFUNCTION()
     void OnJoinSessionComplete(FName SessionName, EOnJoinSessionCompleteResult::Type Result);
 
-    
 private:
     void SetupOnlineSession();
+    void UpdateMainMenuServerList(const TArray<FServerData>& ServerData);
 
     IOnlineSessionPtr SessionInterface;
     TSharedPtr<FOnlineSessionSearch> SessionSearch;
 
+    // Name of server we intend to create
     FString DesiredServerName;
-
-    // Updates the server list in MainMenu
-    void UpdateMainMenuServerList(const TArray<FServerData>& ServerData);
 };
